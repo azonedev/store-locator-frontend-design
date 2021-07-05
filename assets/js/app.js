3a5c8a650ab3735671cf7bdde11f255c0d40753e
azonedev <abdullah914115@gmail.com>
2021-07-05 12:45:39 +0600

    Full screen search box added on hear

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,14 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+// Open the full screen search box
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
